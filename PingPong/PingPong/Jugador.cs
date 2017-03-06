50115f33ed0bde3780befd8efe9aa617505961e1
45858000w <45858000w@aula73-32>
2017-03-06 19:33:47 +0100

    crear proyecto, archivo de instrucciones y clase jugador basica

---
+++ PingPong/PingPong/Jugador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong
+{
+    public class Jugador
+    {
+        public string nombre { get; set; }
+
+        public Jugador() { }
+
+        public Jugador (string name)
+        {
+            nombre = name;
+        }
+
+        public override string ToString()
+        {
+            return "Jugador -> "+nombre;
+        }
+    }
+}
