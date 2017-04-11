2524130bdf2f02d9fd30102d0aee0a5a615cf3b7
cristian javier <cristianjavier2394@hotmail.com>
2017-04-11 22:21:55 +0200

    archivo de cosas por hacer

---
+++ PingPong/PingPong/TODO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong
+{
+    class TODO
+    {
+        //falta cargar los datos de una liga desde la BBDD
+        //borrar una liga (cancelar) de la bbdd ya empezada en el programa
+    }
+}
