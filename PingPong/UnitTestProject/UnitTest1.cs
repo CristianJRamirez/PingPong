448ef4bd822473bb9b0f8d198db53ec53396b5ba
45858000w <45858000w@aula73-32>
2017-05-02 16:39:25 +0200

    Añadido Parte de test unitario

---
+++ PingPong/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PingPong;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string nombre = "David";
+            string puntos = "5";
+
+            Jugador j = new Jugador(nombre, puntos);
+
+            Assert.AreEqual(j.nombre, nombre);
+            Assert.AreEqual(j.puntos, puntos);
+
+            Assert.IsNotNull(j);
+
+            Assert.IsNull(j.ID);
+        }
+    }
+}
